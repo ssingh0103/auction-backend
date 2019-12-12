79f5b93b09bd8ac8dfe3ce13c75c7d9e9d581154
Vagif <vaghayev@xactlycorp.com>
2019-12-12 12:02:23 -0500

    email

--- item/controller.js
+++ item/controller.js
@@ -1,6 +1,9 @@
 
 const Item = require('./model');
 const BidHistory = require('../bidHistory/model');
+
+var nodemailer = require('nodemailer');
+
 exports.getAll = async (req,res)=>{
     try{
         let items = await Item.find();
@@ -64,8 +67,17 @@ exports.updateOne =async (req,res)=>{
     }
 }
 
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'margarette.dickens@ethereal.email',
+        pass: 'JrCVsEy6p1GnMRaEXf'
+    }
+});
+
+  
 exports.updateHighBid = async (req,res)=>{
-    console.log(req.body);
     try{
         let id = req.params.id;
         let newBidHistory = new BidHistory({
@@ -75,7 +87,31 @@ exports.updateHighBid = async (req,res)=>{
             itemId: id
             });
         const up = await Item.findByIdAndUpdate(id,req.body,{new: true});
+        const b = await  BidHistory.find({itemId:id})
+        let max =0;
+        let lastOne =null;
+        b.forEach(item=>{
+            if(item.highestBid>max){
+                max = item.highestBid
+                lastOne = item.highestBidderEmail
+            }
+        })
+      
+        var mailOptions = {
+            from: 'agavaqif@gmail.com',
+            to: lastOne,
+            subject: 'Sending Email using Node.js',
+            text: 'That was easy!'
+          };
         let obj = await newBidHistory.save();
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
         res.send(obj);
 
     }catch(e){
